646f1c030ba1dbec57a34efb3009108031e0a641
Igor Sthanny <igor.nascimento@popcode.com.br>
2022-01-04 19:44:17 -0300

    feat: create exemplo

---
+++ example/android/app/src/main/kotlin/br/com/sthaynny/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.sthaynny.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:adaptable_screen/adaptable_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  AdaptableScreenUtils.init(
+    heightUIDisign: 1000,
+    widthUIDisign: 956,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(10.r),
+              ),
+              height: 100.h,
+              width: 100.w,
+              child: Center(
+                child: Text(
+                  "Text Exemplo",
+                  style: TextStyle(fontSize: 15.ssp),
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(10.r),
+              ),
+              height: 50.h,
+              width: 100.w,
+              child: Center(
+                child: Text(
+                  "Text Exemplo",
+                  style: TextStyle(fontSize: 15.sp),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
