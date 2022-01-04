f6567bbd70025e66ca5536040bc04c5f3d8acb8d
Igor Sthanny <igor.nascimento@popcode.com.br>
2022-01-04 18:48:24 -0300

    feat: add documentary comments

--- lib/src/adaptable_screen_utils.dart
+++ lib/src/adaptable_screen_utils.dart
@@ -14,7 +14,7 @@ class AdaptableScreenUtils {
   final Size _defaultSize = const Size(375, 812);
 
   /// Representing the main window for applications where there is only one
-  /// window, such as applications designed for single-display mobile devices or
+  /// window, such as applications designed for single-display mobile windows or
   ///  the main window to which this binding is bound.
   SingletonFlutterWindow get _window =>
       WidgetsBinding.instance?.window ?? ui.window;
@@ -29,14 +29,14 @@ class AdaptableScreenUtils {
     return MediaQueryData.fromWindow(_window).size.height;
   }
 
-  /// Represents the actual device scaling factor.
-  double get factorTextScaleRealDevice {
+  /// Represents the actual window scaling factor.
+  double get factorTextScaleRealwindow {
     return MediaQueryData.fromWindow(_window).textScaleFactor;
   }
 
   // Represents the scale factor with the range up to 1.6
   double get factorTextScale {
-    return factorTextScaleRealDevice <= 1.6 ? factorTextScaleRealDevice : 1.6;
+    return factorTextScaleRealwindow <= 1.6 ? factorTextScaleRealwindow : 1.6;
   }
 
   /// The ratio of the actual dp to the design draft px
@@ -45,6 +45,7 @@ class AdaptableScreenUtils {
   /// The ratio of the actual dp to the design draft px
   double get scaleHeiht => height / _defaultSize.height;
 
+  /// text scaled based on width.
   double get textScale => scaleWidth;
 
   /// The offset from the top
@@ -54,24 +55,25 @@ class AdaptableScreenUtils {
   double get heightBottomBar =>
       MediaQueryData.fromWindow(_window).padding.bottom;
 
-  /// Maximum Device Height and Width Sizes
+  /// Maximum window Height and Width Sizes
   double get lengthMax {
     return max(height, width);
   }
 
-  /// Scales radius based on minimum device sizes
-  double radius(double radius) => radius * lengthMin;
-
-  get lengthMin {
+  /// Minimum window Height and Width Sizes
+  double get lengthMin {
     return min(height, width);
   }
 
-  /// Scaled height based on device size.
+  /// Scales radius based on minimum window sizes
+  double radius(double radius) => radius * lengthMin;
+
+  /// Scaled height based on window size.
   double steppedHeight(double height) {
     return height * scaleHeiht;
   }
 
-  /// Scaled wigth based on device size.
+  /// Scaled width based on window size.
   double steppedWidth(double width) {
     return width * scaleWidth;
   }
@@ -79,8 +81,8 @@ class AdaptableScreenUtils {
   /// Adjusting font according to the scaling factor, the scaling can be dynamic or fixed.
   double setFont(double fontSize, {bool autoScale = false}) {
     if (autoScale) {
-      return fontSize * textScale * factorTextScale / factorTextScaleRealDevice;
+      return fontSize * textScale * factorTextScale / factorTextScaleRealwindow;
     }
-    return (fontSize * textScale) / factorTextScaleRealDevice;
+    return (fontSize * textScale) / factorTextScaleRealwindow;
   }
 }
--- lib/src/size_extesion.dart
+++ lib/src/size_extesion.dart
@@ -1,10 +1,20 @@
 part of adaptable_screen;
 
 extension AdaptableScreen on num {
+  /// Adding scaling in height based on window size.
   double get h => AdaptableScreenUtils._instance.steppedHeight(toDouble());
+
+  /// Adding scaling in width based on window size.
   double get w => AdaptableScreenUtils._instance.steppedWidth(toDouble());
+
+  /// Adding scaling in text based on window size.
   double get sp => AdaptableScreenUtils._instance.setFont(toDouble());
+
+  /// By scaling the text based on the window size, this function can change the
+  ///  font size as the window changes size.
   double get ssp =>
       AdaptableScreenUtils._instance.setFont(toDouble(), autoScale: true);
+
+  /// Adding radius scaling based on minimum window size.
   double get r => AdaptableScreenUtils._instance.radius(toDouble());
 }
